b2e9d40d8ddb255fe8f937f76c7355735b36d01a
panjiprann <panjipranantya20@gmail.com>
2025-10-21 10:12:35 +0700

    add API endpoint to fetch mahasiswa data from MySQL

--- index.js
+++ index.js
@@ -30,3 +30,13 @@ db.connect((err) => {
     console.log('Connection to MySQL successful!');
 });
 
+app.get('/api/mahasiswa', (req, res) => {
+    db.query('SELECT * FROM mahasiswa', (err, results) => {
+        if (err) {
+            console.error('Error executing query:', + err.stack);
+            res.status(500).send('Error fetching users');
+            return;
+        }
+        res.json(results);
+    });
+});
